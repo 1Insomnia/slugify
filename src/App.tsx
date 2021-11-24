430c8633a731ac01f90324f0e9a80ef10b5a7e55
Jérémy <jeremy.pro.lp@gmail.com>
2021-11-24 13:43:10 +0100

    Add result

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import UserInput from './components/UserInput'
 
 interface IState {
   result: string
+  copy: string
 }
 
 function App() {
   const [result, setResult] = useState<IState['result']>('')
+  const [copy, setCopy] = useState<IState['copy']>('')
 
   return (
     <div className="font-sans text-dark bg-light min-h-screen">
       <Hero />
-      <UserInput setResult={setResult} />
-      <Result result={result} />
+      <UserInput result={result} setResult={setResult} />
+      <Result result={result} setCopy={setCopy} />
     </div>
   )
 }
